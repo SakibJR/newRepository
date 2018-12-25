e5f916eee049f7fc07457a4975654a70fa2b4745
sakibjr <dollarjunior03@gmail.com>
2018-12-25 10:47:05 +0600

    Added Variables

---
+++ JavaFirstLevel/src/Variables.java
@@ -0,0 +1,38 @@
+//main class
+public class Variables{
+
+    //main function
+    public static void main(String[] args){
+
+        //All types of variables goes here
+        boolean booleanVar;     //true or false
+        byte byteVariables;     //-127 to 128
+        short shortVariables;   //-32,768 to 32,767
+        int intVariables;       //- 2,147,483,648 (-2^31) to 2,147,483,647 (2^31 -1)
+
+        //In this data types we have to put the first letter in the end
+
+        //long --> longVariables = 5493845934593845L;
+        long longVariables;     //range  -9,223,372,036,854,775,808(-2^63) to 9,223,372,036,854,775,807(2^63 -1)
+
+        //float --> floatVariables = 4545.44;
+        float floatVariables;      //32 bit
+
+        //double --> longVariables = 6656.65665632352;
+        double doubleVariables;     // 64 bit;
+
+
+        //char charVariables = "a"
+        char charVariables = 'a'; // only store can one work.
+
+        System.out.println(charVariables);
+
+
+    }
+
+}
+
+
+// go to wwww.javapoint.com for more update or details.
+
+//Java Operator presedence -->>  go to this linkl: https://www.javatpoint.com/operators-in-java
